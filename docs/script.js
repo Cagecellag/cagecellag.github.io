65fdbc60ad069c24f844e7972f66d9127121353e
CageCell <nokesko@outlook.com>
2025-08-19 20:10:39 +0200

    stest

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.getElementById("open-btn");
+  const closeBtn = document.getElementById("close-btn");
+  const sidebar = document.getElementById("sidebar");
+
+  openBtn.addEventListener("click", () => {
+    sidebar.classList.add("show");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("show");
+  });
+});
