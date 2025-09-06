6c09325505665c109a68dbb17b01ab78e7cbbc9b
CageCell <nokesko@outlook.com>
2025-09-06 12:06:45 +0200

    Page transition

--- Scripting/script.js
+++ Scripting/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeBtn = document.getElementById("close-btn");
   const sidebar = document.getElementById("sidebar");
   const overlay = document.getElementById("overlay");
-
+  const overlay2 = document.getElementById("overlay2");
 
   openBtn.addEventListener("click", () => {
     sidebar.classList.add("show");
@@ -13,10 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
     sidebar.classList.remove("show");
   });
 
+      setTimeout(() => {
+    overlay2.classList.remove("active");
+  }, 100);
+
 
 window.transitionPage = function (e) {
     e.preventDefault();
     const target = e.target.href;
+
+
 
     // Close sidebar if open
     if (sidebar.classList.contains("show")) {
