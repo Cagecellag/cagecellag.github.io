4d66541af02a1689c20f9fe9490aed84af74cfb3
CageCell <nokesko@outlook.com>
2025-11-09 00:07:13 +0100

    fixed the load and unload button

--- content/content.js
+++ content/content.js
@@ -1,20 +1,18 @@
 const btn = document.getElementById("load-ppt");
 const container = document.getElementById("ppt-container");
-let loaded = false;
 
 btn.addEventListener("click", () => {
-  if (!loaded) {
+     const isActive = btn.classList.toggle("active");
+
+ if (isActive) {
     container.innerHTML = `
-    <iframe 
+        <iframe 
         id="Bombay_PPP" 
         src="https://1drv.ms/p/c/7e906219fa19eb95/IQQYKa1E75O7TZ6j1z7Astv-AZjy4f_2CUw3OpgtOHm07Wk" 
         frameborder="0" scrolling="no">
-    </iframe>`;
-    btn.textContent = "Unload presentation";
-    loaded = true;
+        </iframe>
+    `;
   } else {
-    container.innerHTML = ""; // clears and collapses space
-    btn.textContent = "Load presentation";
-    loaded = false;
+    container.innerHTML = "";
   }
 });
