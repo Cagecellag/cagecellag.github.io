4ef8aff98efb934bddd51e8cdef6572408b6acd8
CageCell <nokesko@outlook.com>
2025-12-07 23:44:37 +0100

    conent updates

--- content/content.js
+++ content/content.js
@@ -9,9 +9,10 @@ const isActive = btn.classList.toggle("active");
 if (isActive) {
     container.innerHTML = `
         <iframe 
-        id="Bombay_PPP" 
         src="https://1drv.ms/p/c/7e906219fa19eb95/IQQYKa1E75O7TZ6j1z7Astv-AZjy4f_2CUw3OpgtOHm07Wk" 
-        frameborder="0" scrolling="no">
+        class="bombay-embed"
+        loading="lazy"
+        title="Presentation">
         </iframe>
     `;
   } else {
